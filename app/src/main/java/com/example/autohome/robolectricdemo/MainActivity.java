7137b7d02b98af8ca4b7a17c51a4ddb8dfd7b26c
954324919 <954324919@qq.com>
2016-06-04 15:43:04 +0800

    第三次提交

--- app/src/main/java/com/example/autohome/robolectricdemo/MainActivity.java
+++ app/src/main/java/com/example/autohome/robolectricdemo/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.autohome.robolectricdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private Button login;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         login=(Button)findViewById(R.id.login);
+        imageView=(ImageView)findViewById(R.id.image);
     }
 }
